8a3fb4ffed836a8390fb1db72b6c14ee59629be0
hipangsu <hipangsu@gmail.com>
2022-04-05 22:18:06 +0900

    1

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -1,3 +1,34 @@
-const popup = {
-  
+
+const popUpLayer = {
+  pensionBtn : null,
+  futureFundsBtn : null,
+
+  pensionPopupLayer : function() {
+    const layer = document.getElementById('pension_popup_layer');
+
+    layer.style.display="none";
+  },
+
+  futureFundsPopupLayer : function() {
+    const layer = document.getElementById('future_funds_popup_layer');
+
+    layer.style.display="none";
+  },
+
+  init : function() {
+    const o = this;
+    o.pensionBtn = document.getElementById('pension_popup_btn');
+    o.futureFundsBtn = document.getElementById('future_funds_popup_btn');
+
+    o.pensionBtn.addEventListener('click', function() {
+      o.pensionPopupLayer();
+    })
+
+    o.futureFundsBtn.addEventListener('click', function() {
+      o.futureFundsPopupLayer();
+    })
+
+  }
+
 }
+popUpLayer.init();
