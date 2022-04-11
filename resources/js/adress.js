deffd6c3cc06aacc2bdbc435f19a1b6b1fff1e73
hipangsu <hipangsu@gmail.com>
2022-04-11 17:28:09 +0900

    1

---
+++ resources/js/adress.js
@@ -0,0 +1,10 @@
+
+function adaptive() {
+
+  if(window.screen.width > 750 ) {
+    window.location.href = "http://127.0.0.1:5500/mirae_asset/page/pc_miare_asset.html";
+  }
+}
+
+
+window.adaptive();
